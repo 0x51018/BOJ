93e3531af078c998fb40a75c49c827bee71f1d65
0x51018 <younhj1018@gmail.com>
2024-11-15 11:15:04 +0900

    2024-11-14-23:59

--- 2751.cc
+++ 2751.cc
@@ -1,1 +1,24 @@
-// 수 100만개 정렬하는 문제. 중복은 없음. merge sort로 풀리려나..?
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, arr[1000000], arrl[500000], arrr[500000];
+
+// void merge(){
+
+// }
+
+// void merge_sort(int *arr, int left, int right){
+//     if(left < right){
+//         int mid = (left + right) / 2;
+//         merge_sort(arr, left, mid);
+//         merge_sort(arr, mid+1, right);
+//         merge(arr, left, mid, right);
+//     }
+// }
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr+n);
+    for(int i = 0; i < n; i++) cout << arr[i] << '\n';
+}
